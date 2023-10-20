8547f6ec91a0ef5cc295a9a2d24267f7a40bfab0
Ali Cicek <ciceka801@gmail.com>
2023-10-20 14:24:18 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  let name = "Ali";
+</script>
+
+<main>
+  <h1>Welcome to SvelteKit</h1>
+  <h2>Hello {name}</h2>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</main>
